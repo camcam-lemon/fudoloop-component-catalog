21dba9d134c45bff7a039e2ca1bdb8ce9ba078e3
camcam-lemon <omega.camcamlemon@gmail.com>
2020-02-05 18:05:12 +0900

    Updates

---
+++ precache-manifest.7e218538c1439f9e8867bbb9fc961dad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e527128760456231293973c4e074cf",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.4df668d7afeacd348bcd.bundle.js"
+  },
+  {
+    "url": "runtime~main.4df668d7afeacd348bcd.bundle.js"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.4df668d7afeacd348bcd.bundle.js"
+  },
+  {
+    "url": "vendors~main.4df668d7afeacd348bcd.bundle.js.LICENSE"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.06c7362fd46570e944742cb6ed8f2281.js"
+  "precache-manifest.7e218538c1439f9e8867bbb9fc961dad.js"
 );
 
 self.addEventListener('message', (event) => {
