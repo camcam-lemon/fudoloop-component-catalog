1b6997433ac76dab6e01e92c2ed4354d4c166864
camcam-lemon <omega.camcamlemon@gmail.com>
2020-02-20 01:05:17 +0900

    :tada: フォントのプリセットを作成

---
+++ src/fonts.ts
@@ -0,0 +1,9 @@
+const FONT_FAMILY = `font-family: Hiragino Sans, 'Hiragino Kaku Gothic ProN', 'メイリオ', sans-serif;`;
+
+const Fonts = {
+  300: `${FONT_FAMILY} font-weight: 300;`,
+  400: `${FONT_FAMILY} font-weight: 400;`,
+  500: `${FONT_FAMILY} font-weight: 500;`,
+};
+
+export default Fonts;
