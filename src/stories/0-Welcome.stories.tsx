0f49ae94d712c9a8b1fc654b7083dc699a93e176
camcam-lemon <omega.camcamlemon@gmail.com>
2020-03-11 00:10:03 +0900

    :books: 先頭ページの文言を作成

--- src/stories/0-Welcome.stories.tsx
+++ src/stories/0-Welcome.stories.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import { linkTo } from '@storybook/addon-links';
-import { Welcome } from '@storybook/react/demo';
+import styled from 'styled-components';
+import { NAVY } from '../colors';
+import Fonts from '../fonts';
+
+const Description = () => (
+  <Article>
+    <h1>コンポーネントカタログ</h1>
+    <Note>fudoloop内で使用されているコンポーネントの一覧を記載しています。</Note>
+    <Note>コンポーネントは全てReact + TypeScript + styled-componentsで構成。</Note>
+  </Article>
+);
 
 export default {
   title: 'Welcome',
-  component: Welcome,
+  component: Description,
 };
 
-export const ToStorybook = () => <Welcome showApp={linkTo('Button')} />;
+export const ToStorybook = () => <Description />;
 
 ToStorybook.story = {
-  name: 'to Storybook',
+  name: 'Fudoloopコンポーネントカタログ',
 };
+
+const Article = styled.article`
+  ${Fonts['300']}
+  margin-left: -144px;
+  margin-top: -40px;
+  display: block;
+  color: ${NAVY.default};
+  line-height: 1.4;
+`;
+const Note = styled.p`
+  padding: 0;
+  margin: 0.5rem 0;
+`;
