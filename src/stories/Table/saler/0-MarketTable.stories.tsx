95fb92dcda484c9aaf57b5c0d01a3880f2e1a4a2
camcam-lemon <omega.camcamlemon@gmail.com>
2020-04-03 11:29:23 +0900

    :books: 市況テーブルのストーリーを追加

--- src/stories/Table/saler/0-MarketTable.stories.tsx
+++ src/stories/Table/saler/0-MarketTable.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import MarketTable, { MarketTableProps } from '../../../components/Table/saler/MarketTable';
+import { useState, useCallback } from '@storybook/addons';
+import MarketTable, { MarketTableProps, Data } from '../../../components/Table/saler/MarketTable';
+import { Vertical, Column } from '../../Container';
 
 export default {
   title: 'Table/MarketTable',
@@ -46,17 +48,84 @@ const MOCK_DATA: MarketTableProps['renderData'] = [
   },
 ];
 
-export const 市況テーブル = () => (
-  <div
-    style={{
-      background: 'black',
-      width: '800px',
-      justifyContent: 'center',
-      display: 'flex',
-      alignItems: 'center',
-      height: '300px',
-    }}
-  >
-    <MarketTable renderData={MOCK_DATA} />
-  </div>
-);
+export const 市況テーブル = () => <MarketTable renderData={MOCK_DATA} />;
+
+export const データがない時 = () => <MarketTable renderData={[]} />;
+
+export const 組み込み = () => {
+  const [dataList, setDataList] = useState(MOCK_DATA);
+  const [targetData, setTargetData] = useState<Data | null>(null);
+  const [editable, setEditable] = useState(false);
+  const [editIndex, setEditIndex] = useState(-1);
+  const onClickEdit = useCallback<MarketTableProps['onClickEdit']>(
+    (e, index) => {
+      e.stopPropagation();
+      setEditable(prev => !prev);
+      setEditIndex(index);
+      setTargetData(dataList[index]);
+    },
+    [dataList],
+  );
+  const onClickEditComplete = useCallback<MarketTableProps['onClickEditComplete']>(
+    (e, index, values) => {
+      const { standard, totalAmount, row, middle, high, comment } = values;
+      e.stopPropagation();
+      setEditable(prev => !prev);
+      setEditIndex(-1);
+      setTargetData(null);
+
+      const prevData = dataList.map((key, i) =>
+        i === index
+          ? {
+              ...key,
+              standard,
+              totalAmount: Number(totalAmount),
+              prices: {
+                row: Number(row),
+                middle: Number(middle),
+                high: Number(high),
+              },
+              comment,
+            }
+          : key,
+      );
+      setDataList(prevData);
+    },
+    [dataList],
+  );
+
+  const onCancel = useCallback<MarketTableProps['onCancel']>((e, index) => {
+    e.stopPropagation();
+    setEditable(prev => !prev);
+    setEditIndex(-1);
+    setTargetData(null);
+  }, []);
+
+  return (
+    <div>
+      <Column>
+        <div>モード：{editable ? '編集中' : '閲覧中'}</div>
+        <div>編集行：{editIndex === -1 ? '未洗濯' : editIndex + 1}</div>
+        <div>
+          <div>選択されたデータ</div>
+          <ul>
+            <li>id: {targetData?.id}</li>
+            <li>作物名: {targetData?.name}</li>
+            <li>規格: {targetData?.standard}</li>
+            <li>全体量: {targetData?.totalAmount}</li>
+            <li>安値: {targetData?.prices.row}</li>
+            <li>中値: {targetData?.prices.middle}</li>
+            <li>高値: {targetData?.prices.high}</li>
+            <li>傾向： {targetData?.comment}</li>
+          </ul>
+        </div>
+      </Column>
+      <MarketTable
+        renderData={dataList}
+        onClickEdit={onClickEdit}
+        onClickEditComplete={onClickEditComplete}
+        onCancel={onCancel}
+      />
+    </div>
+  );
+};
