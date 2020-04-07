ba34978321c83207484bddc760d97822b1b1778f
camcam-lemon <omega.camcamlemon@gmail.com>
2020-04-07 16:23:20 +0900

    :+1: titleを編集

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import TableComponent from './TableComponent';
